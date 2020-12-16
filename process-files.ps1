0c88228b9d1a6a45fdd41dd268e5ab42a87b99cf
Partho P. Das <parthopdas@live.com>
2020-12-17 08:49:05 +1000

    Renaming files + adding text lines + Notify discord - 1

--- process-files.ps1
+++ process-files.ps1
@@ -14,18 +14,22 @@ Import-Module "$PSScriptRoot\PSExtractTexts.psm1" -Force
 
 function CheckLines {
   param(
-    $Nodes,
+    $TextLines,
     $IncludedLines,
     $ExcludedLines
   )
 
-  if (-not (($Nodes.Length -eq $includedLines.Length) -and ($Nodes.Length -eq $excludedLines.Length))) {
+  if ($TextLines.Length -eq 0) {
+    return "No text lines"
+  }
+
+  if (-not (($TextLines.Length -eq $IncludedLines.Length) -and ($TextLines.Length -eq $ExcludedLines.Length))) {
     return "Different number of lines"
   }
 
   [array] $mismatchLengthLines =
-    0..$Nodes.Length | Where-Object {
-      $Nodes[$_].InnerText.Length -ne ($includedLines[$_].Length + $excludedLines[$_].Length)
+    0..$TextLines.Length | Where-Object {
+      $TextLines[$_].Length -ne ($IncludedLines[$_].Length + $ExcludedLines[$_].Length)
     }
 
   if ($mismatchLengthLines.Length) {
@@ -66,15 +70,21 @@ function ProcessFile {
       $excludedLines += $_[1]
     }
 
-    $includedFilePath = [io.path]::ChangeExtension($dstFilePath, "included.txt")
+    [array] $textLines = $nodes | ForEach-Object { $_.Node.InnerText }
+
+    $textFilePath = "$dstFilePath.txt"
+    $textLines | Out-File -FilePath $textFilePath -Encoding utf8BOM
+
+    $includedFilePath = "$dstFilePath.included.txt"
     $includedLines | Out-File -FilePath $includedFilePath -Encoding utf8BOM
 
-    $excludedFilePath = [io.path]::ChangeExtension($dstFilePath, "excluded.txt")
+    $excludedFilePath = "$dstFilePath.excluded.txt"
     $excludedLines | Out-File -FilePath $excludedFilePath -Encoding utf8BOM
 
-    if (-not (CheckLines $nodes $includedLines $excludedLines)) {
+    $failures = CheckLines $textLines $includedLines $excludedLines
+    if ($failures -ne "") {
       Write-Host "[$($includedLines.Length)/$($excludedLines.Length) lines]`t" -NoNewline
-      Write-Host "[Check failed]" -ForegroundColor Red -NoNewline
+      Write-Host "[Check failed] [$failures]" -ForegroundColor Red -NoNewline
       Write-Host (" [{0:mm\:ss\.fff}]s" -f ([datetime]::Now - $now))
       $False
     } else {
@@ -118,16 +128,15 @@ function ProcessSegment {
     $files[$firstFile..$lastFile]
     | ProcessFile $SrcDir $DstDir
   $failedCount = ($results | Where-Object { -not $_ }).Length
-  Write-Host ("Summary: Duration {0:mm\:ss\.fff} Complete {1}, Failed {2}." -f ([datetime]::Now - $now), $results.Length, $failedCount)
+  Write-Host ("Summary: Duration {0:mm\:ss\.fff} Total {1}" -f ([datetime]::Now - $now), $results.Length) -ForegroundColor White -NoNewline
+  Write-Host (" Completed {0}" -f ($results.Length - $failedCount)) -ForegroundColor Green -NoNewline
+  Write-Host (" Failed {0}" -f $failedCount) -ForegroundColor Red -NoNewline
+  Write-Host "."
 }
 
 # Test cases
 # dir "D:\src\dpt\cst\cscd\abh01a.att0.xml" | ProcessFile $SrcDir $DstDir # Has 830 nodes
 # dir "D:\src\dpt\cst\cscd\vin07t.nrf9.xml" | ProcessFile $SrcDir $DstDir # Has 1 node
 # dir "D:\src\dpt\cst\cscd\s0201m.mul0.xml" | ProcessFile $SrcDir $DstDir
-
-#dir "D:\src\dpt\cst\cscd\abh03m10.mul14.xml" | ProcessFile $SrcDir $DstDir # Has 830 nodes
-
-#ProcessDirectory $SrcDir $DstDir $Filter
 
 ProcessSegment $SrcDir $DstDir $Segment $TotalSegments $Filter
